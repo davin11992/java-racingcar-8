5891500df65bced39ac3812cac03db13f0da06ab
DavinLee <davin11992@gmail.com>
2025-10-27 00:43:35 +0900

    test: CarsTest 유닛테스트 추가

---
+++ src/test/java/racingcar/model/domain/CarsTest.java
@@ -0,0 +1,37 @@
+package racingcar.model.domain;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CarsTest {
+    @Test
+    void 이름_리스트로_Cars_생성() {
+        List<String> names = List.of("pobi", "woni", "davin");
+        Cars cars = new Cars(names);
+
+        assertThat(cars.getCars()).hasSize(3);
+        assertThat(cars.getCars())
+                .extracting(Car::getName)
+                .containsExactly("pobi", "woni", "davin");
+    }
+
+    @Test
+    void 랜덤이_4가_들어오면_모든_차_전진() {
+        assertRandomNumberInRangeTest(() -> {
+            Cars cars = new Cars(List.of("pobi", "woni"));
+            List<Car> moved = cars.moveAll();
+
+            assertThat(moved)
+                    .allSatisfy(car -> assertThat(car.getPosition()).isEqualTo(1));
+        }, 4, 4);
+    }
+}
